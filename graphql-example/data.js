b6eab7ed2c12bebd3e78c77505dd8088f89c7b3f
XavierQu23 <quishpexavier27@gmail.com>
2025-01-12 23:40:06 -0500

    first commit

---
+++ graphql-example/data.js
@@ -0,0 +1,8 @@
+// data.js
+const books = [
+    { id: 1, title: "1984", author: "George Orwell" },
+    { id: 2, title: "Brave New World", author: "Aldous Huxley" },
+  ];
+  
+  module.exports = { books };
+  
